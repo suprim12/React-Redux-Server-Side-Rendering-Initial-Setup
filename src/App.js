9849089f8d4c4a55cabc5bc4240ed8b1cc449717
NepalCodeTv <62992145+NepalCodeTv@users.noreply.github.com>
2020-04-23 00:00:54 +0545

    Changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello react redux from server side.</h1>
+        <p>View Source "ctrl + U"</p>
+        <p>Redux Devtools to see state</p>
       </header>
     </div>
   );
